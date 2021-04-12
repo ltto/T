c31ca5405aa186c707ce1272c51a57d6e28c88fc
qianxiaoxiao <121172722@qq.com>
2021-04-12 15:55:16 +0800

    www add *

--- www/main/main.go
+++ www/main/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/ltto/T/www"
 	"github.com/ltto/T/www/rest"
 )
 
 func main() {
 	www.Get("/hello", func(c *www.Context) interface{} {
-		return rest.ReturnFile("/Users/liutongtong/gocode/T/www/main/rgb.png")
+		return rest.ReturnRedirect("https://www.baidu.com")
 	})
 
+	www.Get("/ok", func(c *www.Context) string {
+		return "OK"
+	})
+	www.Get("/json", func(c *www.Context) gin.H {
+		return gin.H{"json": "ok"}
+	})
 	www.Run()
 }
